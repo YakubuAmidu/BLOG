4b7f7c42272dd3ec45fa657b6782614c77fd71d4
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-03-29 00:10:13 -0700

    Updating Blog

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -4,25 +4,32 @@ import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import axios from 'axios';
 
 class Blog extends Component {
-    render () {
-        return (
-            <div>
-                <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
-                </section>
-                <section>
-                    <FullPost />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
-        );
-    }
+  componentDidMount() {
+    axios.get('http://jsonplaceholder.typicode.com/posts').then((response) => {
+      console.log(response);
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <section className='Posts'>
+          <Post />
+          <Post />
+          <Post />
+        </section>
+        <section>
+          <FullPost />
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Blog;
