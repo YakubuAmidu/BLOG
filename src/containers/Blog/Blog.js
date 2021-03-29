d127df06a1f74cdd1e3d6e1b1615b07dd5d24e11
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-03-29 01:04:46 -0700

    Updating changes

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,6 +9,7 @@ import axios from 'axios';
 class Blog extends Component {
   state = {
     posts: [],
+    selectedPostId: null,
   };
 
   componentDidMount() {
@@ -25,15 +26,19 @@ class Blog extends Component {
     });
   }
 
+  postSelectedHandler = (id) => {
+    this.setState({ selectedPostId: id });
+  };
+
   render() {
     const posts = this.state.posts.map((post) => {
-      return <Post key={post.id} title={post.title} />;
+      return <Post key={post.id} title={post.title} author={post.author} />;
     });
     return (
       <div>
         <section className='Posts'>{posts}</section>
         <section>
-          <FullPost />
+          <FullPost id={this.state.selectedPostId} />
         </section>
         <section>
           <NewPost />
