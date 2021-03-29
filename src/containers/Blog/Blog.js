838319dc1b61e06967eac678a46b675e39322db1
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-03-29 00:23:52 -0700

    Updating Blog

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -7,20 +7,24 @@ import './Blog.css';
 import axios from 'axios';
 
 class Blog extends Component {
+  state = {
+    posts: [],
+  };
+
   componentDidMount() {
     axios.get('http://jsonplaceholder.typicode.com/posts').then((response) => {
-      console.log(response);
+      this.setState({ posts: response.data });
+      //console.log(response);
     });
   }
 
   render() {
+    const posts = this.state.posts.map((post) => {
+      return <Post />;
+    });
     return (
       <div>
-        <section className='Posts'>
-          <Post />
-          <Post />
-          <Post />
-        </section>
+        <section className='Posts'>{posts}</section>
         <section>
           <FullPost />
         </section>
